363e3a05689a72fd8e4ce000c63b4bf77f8a9b8b
Benjamin Jensen <Benjaminbyvej@gmail.com>
2024-02-27 15:50:35 +0100

    added axios as dependency, changed port, changed "About" to "Answer"

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,11 +8,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Ear Impression Analysis" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/answer">Answer</RouterLink>
       </nav>
     </div>
   </header>
@@ -56,30 +56,4 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ frontend/src/views/AnswerView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="answer">
+    <button v-on:click="getAnswer">What is the answer?</button>
+    <p>{{ answer }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios';
+
+export default {
+  name: 'AnswerDisplay',
+  data() {
+    return {
+      answer: null
+    };
+  },
+  methods: {
+    getAnswer() {
+      axios.get('http://localhost:8000/answer')
+        .then(response => {
+          this.answer = response.data.answer;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  }
+}
+
+</script>
+
+<style>
+</style>
